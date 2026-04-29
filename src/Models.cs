f8fde59ae4f1fb14f8d2670da9432b1097d3da95
Daniel Fang <qkfang@hotmail.com>
2026-04-29 22:07:56 +1000

    remove

--- src/Models.cs
+++
@@ -1,7 +0,0 @@
-namespace BookMyShow.Functions;
-
-public sealed record Movie(int M_Id, string M_Name, int M_Available);
-
-public sealed record Order(int O_Id, int M_Id, int No_Tickets, int Price);
-
-public sealed record ErrorResponse(string Error);
